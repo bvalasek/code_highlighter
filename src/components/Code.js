a9db1a4f4e502199c71f4162354258a09f77d565
Branislav Valasek <branislav.valasek@infor.com>
2023-03-24 12:25:56 +0100

    Prism library - displaying Line Wrap

--- src/components/Code.js
+++ src/components/Code.js
@@ -8,6 +8,11 @@ import Prism from 'prismjs';
 // Theme generator:
 // http://k88hudson.github.io/syntax-highlighting-theme-generator/www/
 
+// ten pre-wrap in-line style override na Pre a Code si vycital odtialto,
+// ak by si to nemal na Code tak v pripade, ze nezomrazujes Line Numbers
+// tak wrap by nefungoval, takze si to dal aj na Code
+// https://github.com/PrismJS/prism/issues/3419
+
 export default function Code({ code, language, dataLines }) {
     useEffect(() => {
         Prism.highlightAll();
@@ -15,8 +20,8 @@ export default function Code({ code, language, dataLines }) {
     return (
         <div className="Code">
             <h2> Code Syntax Block {language}</h2>
-            <pre className="line-numbers" data-line={dataLines} >
-                <code className={`language-${language}`}>{code}</code>
+            <pre className="line-numbers" data-line={dataLines} style={{ whiteSpace: 'pre-wrap', }}>
+                <code className={`language-${language}`} style={{ whiteSpace: 'pre-wrap', }}>{code}</code>
             </pre>
         </div>
     );
